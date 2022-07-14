776a2bf70e11a45813edb8b7c93428dc836c7b1a
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-07-14 12:34:48 -0400

    updated bootloader sys config

--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -2,21 +2,9 @@
 #include <efilib.h>
 
 EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-	EFI_STATUS Status;
-	EFI_INPUT_KEY Key;
 
-	ST = SystemTable; //Store the system table
+	InitializeLib(ImageHandle, SystemTable);
+	Print(L"String blah blah blah\n\r");
 
-	Status = ST->ConOut->OutputString(ST->ConOut, L"HelloWorld!\n\r"); // Print HelloWorld to the console
-
-	if (EFI_ERROR(Status)) //If printing failed, return
-		return Status;
-
-	Status = ST->ConIn->Reset(ST->ConIn, FALSE); // Empty the Console Input Buffer
-	if (EFI_ERROR(Status))
-		return Status;
-
-	while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY) ; // Poll For Key Input
-
-	return Status; // Exit the UEFI application
+	return EFI_SUCCESS; // Exit the UEFI application
 }
